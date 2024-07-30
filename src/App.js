b423104949ff9ddb869535cb2ac800ce11f0a6d0
Kurtis Edwards <edwards.kurtis@gmail.com>
2024-07-29 20:37:33 -0600

    feat: add reset button functionality and additional styling

--- src/App.js
+++ src/App.js
@@ -13,6 +13,11 @@ function Counter() {
   const [count, setCount] = useState(0);
   const [step, setStep] = useState(1);
 
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
+
   return (
     <>
       <div
@@ -20,6 +25,7 @@ function Counter() {
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
+          marginBottom: '20px',
         }}
       >
         <input
@@ -28,28 +34,56 @@ function Counter() {
           max="10"
           value={step}
           onChange={(e) => setStep(Number(e.target.value))}
+          style={{ marginRight: '10px', width: '300px', height: '10px' }}
         />
-        <span>Step: {step}</span>
+        <span style={{ fontSize: '20px' }}>{step}</span>
       </div>
       <div
         style={{
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
+          marginBottom: '20px',
         }}
       >
-        <button onClick={() => setCount(count - step)}>-</button>
+        <button
+          onClick={() => setCount(count - step)}
+          style={{ fontSize: '20px', padding: '10px 20px' }}
+        >
+          -
+        </button>
         <input
           type="number"
           value={count}
           onChange={(e) => setCount(Number(e.target.value))}
+          style={{ margin: '0 10px', width: '100px', fontSize: '20px' }}
         />
-        <button onClick={() => setCount(count + step)}>+</button>
+        <button
+          onClick={() => setCount(count + step)}
+          style={{ fontSize: '20px', padding: '10px 20px' }}
+        >
+          +
+        </button>
       </div>
 
-      <div>
+      <div
+        style={{
+          textAlign: 'center',
+          fontSize: '26px',
+          fontWeight: 'bold',
+          marginTop: '20px',
+        }}
+      >
         {count} days from today is{' '}
         {new Date(Date.now() + count * 24 * 60 * 60 * 1000).toDateString()}
+      </div>
+      <div>
+        <button
+          onClick={handleReset}
+          style={{ fontSize: '20px', padding: '10px 20px', marginTop: '20px' }}
+        >
+          Reset
+        </button>
       </div>
     </>
   );
